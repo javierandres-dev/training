3ec58380aaa8966d732823a1036d0f00244ac952
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-05-10 02:58:25 -0500

    Update

--- courses/0x00/homework_week_4/scripts/main.js
+++ courses/0x00/homework_week_4/scripts/main.js
@@ -1,15 +1,123 @@
-let theData = [
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-]
-//console.log(theData);
-//console.log(typeof(theData));
-/*
+/**
+ * VARIABLES
+ */
+//let strings = ['Mushrooms', 'Onions', 'Olives', 'Zucchini', 'Pepperoni'];
+//let numbers = [3, 1, 1, 1, 2];
+const btnShow = document.getElementById("btnShow");
+const boxResult = document.getElementById("boxResult");
+const strJsonApiTodos = "https://jsonplaceholder.typicode.com/todos";
+const strJsonApiUsers = "https://jsonplaceholder.typicode.com/users";
+let myStrings = [];
+let myNumbers = [];
+let myArray = [];
+/**
+ * EVENTS
+ */
+eventListeners();
+
+function eventListeners() {
+    btnShow.addEventListener("click", showResult);
+}
+/**
+ * FUNCTIONS
+ */
+function myTestOne() {
+    fetch(strJsonApiTodos)
+        .then(response => response.json())
+        //.then(json => console.log(json))
+        .then(json => searchInTodos(json))
+
+    function searchInTodos(json) {
+        //console.log(json);
+        let size = json.length;
+        let todos = json;
+        let tasks = new Array();
+        let task = new Object();
+        for (let i = 0; i < size; i++) {
+            //console.log(todos[i]);
+            task[i] = {};
+            task[i].userId = todos[i].userId;
+            task[i].completed = todos[i].completed;
+            tasks.push(task[i]);
+        }
+        //console.log(tasks);
+        //console.log(tasks[0]);
+        //console.log(tasks.length);
+        fetch(strJsonApiUsers)
+            .then(response => response.json())
+            //.then(json => console.log(json))
+            .then(json => searchInUsers(json))
+
+
+        function searchInUsers(json) {
+            //console.log(json);
+            let size = json.length;
+            let users = json;
+            let students = new Array();
+            let student = new Object();
+            for (let i = 0; i < size; i++) {
+                //console.log(users[i]);
+                student[i] = {};
+                student[i].id = users[i].id;
+                student[i].name = users[i].name;
+                students.push(student[i]);
+            }
+            //console.log(students);
+            //console.log(students[0]);
+            //console.log(students.length);
+            //console.log(task);
+            let task = 0;
+            for (let i = 0; i < students.length; i++) {
+                //console.log(students[i]);
+                //console.log(students[i].id);
+                //console.log(students[i].name);
+                myStrings.push(students[i].name);
+                console.log(task);
+                for (let j = 0; j < tasks.length; j++) {
+                    //console.log(tasks[j]);
+                    //console.log(tasks[j].userId);
+                    //console.log(tasks[j].completed);
+                    if (students[i].id === tasks[j].userId) {
+                        if (tasks[j].completed === true) {
+                            //console.log(students[i].id);
+                            //console.log(students[i].name);
+                            //console.log(tasks[j].userId);
+                            //console.log(tasks[j].completed);
+                            task += 1;
+                            //console.log(task);
+                            //myNumbers.push(task);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+myTestOne();
+
+function showResult() {
+    boxResult.className += "d-block";
+}
+
+function stringsNumbers(s, n) {
+    for (let i = 0; i < s.length; i++) {
+        //console.log(s[i]);
+        //console.log(n[i]);
+        myArray.push([s[i], n[i]]);
+    }
+}
+stringsNumbers(myStrings, myNumbers);
+//console.log(myArray);
+//console.log(typeof(myArray));
+function getStringNumber() {
+    return (myArray);
+}
+console.log(myStrings);
+console.log(myNumbers);
+console.log(myArray);
+/**
  * Google Charts
-*/
+ */
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {
     'packages': ['corechart']
@@ -27,15 +135,24 @@ function drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Topping');
     data.addColumn('number', 'Slices');
-    data.addRows(theData);
+    data.addRows(getStringNumber()); // Source data to print
+    /*
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+    */
 
     // Set chart options
     var options = {
         'title': 'How Much Pizza I Ate Last Night',
-        'width': 800,
-        'height': 400,
+        'width': 800, // Change size
+        'height': 400, // Change size
         'is3D': true
-    };
+    }; // Enable view 3D
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
