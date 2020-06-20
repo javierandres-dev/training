9521e5b98eba533272e1b26e786837649f381b5b
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-06-19 21:59:27 -0500

    Training

--- exercises/0x07/script.js
+++ exercises/0x07/script.js
@@ -1,12 +1,20 @@
 /**
  * VARIABLES
  */
+const btnCrearUsuario = document.getElementById('btnCrearUsuario');
 /**
  * CLASES
  */
 /**
  * EVENT LISTENERS
  */
+eventListeners();
+function eventListeners(){
+    btnCrearUsuario.addEventListener('click', crearUsuario);
+}
 /**
  * FUNCIONES
  */
+function crearUsuario(){
+    console.log('Works!');
+}
