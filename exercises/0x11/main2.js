6c867cb74797185da11be305335df2b1e76a1a9c
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-07-01 12:59:40 -0500

    Training: exercises/0x11

--- exercises/0x11/main2.js
+++
@@ -1,7 +0,0 @@
-const readKms =  document.getElementById('readKms')
-const showMiles = document.getElementById('showMiles')
-readKms.addEventListener('click', toMiles)
-function toMiles(){
-    let miles = readKms.value * 0.62137
-    showMiles.innerHTML = miles
-}
