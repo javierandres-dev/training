8dfb0d48b846025a6de886a52908ea331abd9722
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-07-16 16:41:29 -0500

    Training: exercises 0x14

--- exercises/0x14/README.md
+++ exercises/0x14/README.md
@@ -6,12 +6,18 @@ Learn React
 
 ## Resources
 ###### :copyright:
-* [Tutorial: Introducción a React](https://es.reactjs.org/tutorial/tutorial.html)
+* [React](https://es.reactjs.org/)
+* [Resumen de la documentación de React y recursos relacionados](https://es.reactjs.org/docs/getting-started.html)
 * [Hola mundo](https://es.reactjs.org/docs/hello-world.html)
+* [Tutorial: Introducción a React](https://es.reactjs.org/tutorial/tutorial.html)
 * [Una re-introducción a JavaScript (Tutorial de JS)](https://developer.mozilla.org/es/docs/Web/JavaScript/Una_re-introducci%C3%B3n_a_JavaScript)
 * [Funciones Flecha](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Funciones/Arrow_functions)
 * [Understanding JavaScript Function Invocation and "this"](https://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/)
 * [React.Component](https://es.reactjs.org/docs/react-component.html)
+* [state y props](https://es.reactjs.org/docs/faq-state.html#what-is-the-difference-between-state-and-props)
+* [Resumen de React por Tania Rascia](https://www.taniarascia.com/getting-started-with-react/)
+* [MDN JavaScript](https://developer.mozilla.org/es/docs/Web/JavaScript)
+* [JavaScript.info](https://javascript.info/)
 * [Extensión de React Devtools para Chrome](https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en)
 * [Extensión de React Devtools Firefox](https://addons.mozilla.org/en-US/firefox/addon/react-devtools/)
 * [Babel is a JavaScript compiler](https://babeljs.io/docs/en/)
--- exercises/0x14/my-app/src/index.js
+++ exercises/0x14/my-app/src/index.js
@@ -235,6 +235,132 @@ function Mailbox(props) {
     </div>
   );
 }
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+function NumberList(props) {
+  const numbers = props.numbers;
+  return (
+    <ul>
+      {numbers.map((number) => (
+        <ListItem key={number.toString()} value={number} />
+      ))}
+    </ul>
+  );
+}
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "0" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("Your favorite flavor is: " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="0" disabled>
+              Choose...
+            </option>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+const scaleNames = { c: "Celsius", f: "Fahrenheit" };
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
+  handleCelsiusChange(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
 class Hello extends React.Component {
   render() {
     function getGreeting(argv) {
@@ -251,6 +377,10 @@ class Hello extends React.Component {
       lastName: "Patarroyo",
     };
     const messages = ["React", "Re: React", "Re:Re: React"];
+    const numbers = [1, 2, 3, 4, 5];
+    //const doubled = numbers.map((number) => number * 2);
+    //console.log(doubled);
+    //const listItems = numbers.map((number) =>  <li>{number}</li>);
     //const name = 'Javi';
     //const element = <div>Hello, {formatName(user)}!</div>;
     //return <>{getGreeting(user)}</>;
@@ -262,7 +392,11 @@ class Hello extends React.Component {
     //return (<Greeting isLoggedIn={false} />);
     //return (<Greeting isLoggedIn={true} />);
     //return (<LoginControl />);
-    return <Mailbox unreadMessages={messages} />;
+    //return <Mailbox unreadMessages={messages} />;
+    //return <ul>{listItems}</ul>
+    //return <NumberList numbers={numbers} />;
+    //return <FlavorForm />;
+    return <Calculator />;
   }
 }
 // ========================================
