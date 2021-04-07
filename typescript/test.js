b5d5720467f2480605c317331c5ee5f0c3e577af
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-06 22:23:23 -0500

    Training

--- typescript/test.js
+++ typescript/test.js
@@ -1,16 +1,16 @@
 var d = document;
-d.addEventListener("DOMContentLoaded", function () {
-    show("Welcome");
+d.addEventListener('DOMContentLoaded', function () {
+    show('Welcome');
     setName();
 });
 var setName = function () {
-    var $form = d.querySelector("form"), $str = d.getElementById("str"), $num = d.getElementById('num'), $list = d.getElementById("list");
-    $form.addEventListener("submit", function (e) {
+    var $form = d.querySelector('form'), $str = d.getElementById('str'), $num = d.getElementById('num'), $list = d.getElementById('list');
+    $form.addEventListener('submit', function (e) {
         e.preventDefault();
         var str = $str.value;
         var num = parseInt($num.value);
         if (str.length === 0 || isNaN(num)) {
-            console.log("stop");
+            console.log('stop');
             return;
         }
         $list.textContent = str + ", " + num;
@@ -18,5 +18,36 @@ var setName = function () {
 };
 var show = function (aString) {
     d.getElementById('test').innerHTML = "" + aString;
-    //console.log(aString);
 };
+console.log('Hello, World!');
+console.log('-- -- -- -- DataTypes -- -- -- --');
+var anyType;
+var numberType = 0;
+var stringType = 'str';
+var arrNumbers = [1, 2, 3, 4];
+var arrStrings = ['a', 'b', 'c'];
+var arrAny = [1, 'a', true, {}, []];
+function types() {
+    console.log(anyType);
+    console.log(numberType);
+    console.log(stringType);
+    console.log(arrNumbers);
+    console.log(arrStrings);
+    console.log(arrAny);
+}
+types();
+console.log('-- -- -- -- Interface -- -- -- --');
+var user1 = { username: 'Mary', password: '1234', nickmane: 'mari' };
+var user2 = { username: 'Jhon', password: '4321' };
+console.log(user1, '\n', user2);
+var action1 = {
+    greet: function () { return console.log('hi'); }
+};
+action1.greet();
+console.log('-- -- -- -- Functions -- -- -- --');
+function fn(a, b) {
+    return a + b;
+}
+console.log(fn(1, 2));
+var afn = function (a, b) { return a * b; };
+console.log(afn(1, 2));
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,27 +1,78 @@
 const d = document;
 
-d.addEventListener("DOMContentLoaded", ()=>{
-  show("Welcome");
+d.addEventListener('DOMContentLoaded', () => {
+  show('Welcome');
   setName();
-  })
+});
 
-const setName = () => {
-  const $form = d.querySelector("form"),
-   $str = d.getElementById("str"),
-   $num = d.getElementById('num'),
-  $list = d.getElementById("list");
-  $form.addEventListener("submit", (e)=>{
+const setName = (): void => {
+  const $form = d.querySelector('form'),
+    $str = d.getElementById('str'),
+    $num = d.getElementById('num'),
+    $list = d.getElementById('list');
+  $form.addEventListener('submit', (e) => {
     e.preventDefault();
     const str: String = (<HTMLInputElement>$str).value;
     const num: number = parseInt((<HTMLInputElement>$num).value);
-    if (str.length === 0 || isNaN(num) ){
-      console.log("stop");
+    if (str.length === 0 || isNaN(num)) {
+      console.log('stop');
       return;
     }
     $list.textContent = `${str}, ${num}`;
-  })
+  });
+};
+
+const show = (aString: string): void => {
+  d.getElementById('test').innerHTML = `${aString}`;
+};
+
+console.log('Hello, World!');
+console.log('-- -- -- -- DataTypes -- -- -- --');
+
+let anyType: any;
+const numberType: number = 0;
+const stringType: string = 'str';
+const arrNumbers: Array<number> = [1, 2, 3, 4];
+const arrStrings: Array<string> = ['a', 'b', 'c'];
+const arrAny: Array<any> = [1, 'a', true, {}, []];
+
+function types(): void {
+  console.log(anyType);
+  console.log(numberType);
+  console.log(stringType);
+  console.log(arrNumbers);
+  console.log(arrStrings);
+  console.log(arrAny);
 }
 
-const  show = (aString: string) => {
-  d.getElementById('test').innerHTML = `${aString}`
+types();
+
+console.log('-- -- -- -- Interface -- -- -- --');
+interface User {
+  username: string;
+  password: string;
+  nickmane?: string;
 }
+
+const user1: User = { username: 'Mary', password: '1234', nickmane: 'mari' };
+const user2: User = { username: 'Jhon', password: '4321' };
+
+console.log(user1, '\n', user2);
+
+interface Action {
+  greet(): void;
+}
+
+const action1: Action = {
+  greet: () => console.log('hi'),
+};
+
+action1.greet();
+
+console.log('-- -- -- -- Functions -- -- -- --');
+function fn(a: number, b: number): number {
+  return a + b;
+}
+console.log(fn(1, 2));
+const afn = (a: number, b: number): number => a * b;
+console.log(afn(1, 2));
