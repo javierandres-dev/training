3ca1aae8e2db5606ee1d73d57bcaa0dd1baaa228
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-12 12:34:58 -0500

    Training

--- javascript/0x09/fizzbuzz.js
+++ javascript/0x09/fizzbuzz.js
@@ -1,5 +1,19 @@
-function fizzbuzz(aNumber){
-  return (aNumber);
+function fizzbuzz(aNumber) {
+  if (typeof aNumber !== 'number') return 'Error: Require a number';
+  if (aNumber === 0) return aNumber;
+  const remainderZero = (divisor, dividend) => divisor % dividend === 0;
+  if (remainderZero(aNumber, 5) && remainderZero(aNumber, 3)) return 'fizzbuzz';
+  if (remainderZero(aNumber, 3)) return 'fizz';
+  if (remainderZero(aNumber, 5)) return 'buzz';
+  return aNumber;
 }
+
+function print(aNumber) {
+  for (let i = 0; i <= aNumber; i++) {
+    console.log(fizzbuzz(i));
+  }
+}
+
+print(16);
 
 module.exports = fizzbuzz;
--- javascript/0x09/fizzbuzz.test.js
+++ javascript/0x09/fizzbuzz.test.js
@@ -1,12 +1,37 @@
 const fizzbuzz = require('./fizzbuzz');
 
 describe('fizzbuzz', () => {
-  test('test', () => {
+  test('it works', () => {
     expect(true).toBe(true);
   });
-  test('should print 1 if recive 1', () => {
+  test('should print "Error: Require a number" if it not receive a number', () => {
+    const expected = 'Error: Require a number';
+    const result = fizzbuzz('1');
+    expect(expected).toBe(result);
+  });
+  test('should print 0 if it receive 0', () => {
+    const expected = 0;
+    const result = fizzbuzz(0);
+    expect(expected).toBe(result);
+  });
+  test('should print 1 if it receive 1', () => {
     const expected = 1;
     const result = fizzbuzz(1);
+    expect(expected).toBe(result);
+  });
+  test('should print "fizzbuzz" if it receives multiples of 5 and 3', () => {
+    const expected = 'fizzbuzz';
+    const result = fizzbuzz(15);
+    expect(expected).toBe(result);
+  });
+  test('should print "fizz" if it receive a multiple of 3', () => {
+    const expected = 'fizz';
+    const result = fizzbuzz(3);
+    expect(expected).toBe(result);
+  });
+  test('should print "buzz" if it receive a multiple of 5', () => {
+    const expected = 'buzz';
+    const result = fizzbuzz(5);
     expect(expected).toBe(result);
   });
 });
