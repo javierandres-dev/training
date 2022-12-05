d9651832fc83d405f107a14c24ec5e3c445cae78
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-12-05 17:27:11 -0500

    training

--- javascript/0x00/number-properties.js
+++ javascript/0x00/number-properties.js
@@ -2,9 +2,12 @@
  * Testing to learn
  */
 let res = undefined;
+res = Number.EPSILON;
+res = Number.MAX_SAFE_INTEGER;
 res = Number.MAX_VALUE;
+res = Number.MIN_SAFE_INTEGER;
 res = Number.MIN_VALUE;
-res = Number.NEGATIVE_INFINITY;
 res = Number.NaN;
+res = Number.NEGATIVE_INFINITY;
 res = Number.POSITIVE_INFINITY;
 console.log(res);
