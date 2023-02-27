c850f0d11990c3f905650366cf38f8f031a5d4a7
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2023-02-26 19:37:34 -0500

    training

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navigation from './components/Navigation';
 
 function App() {
+  console.log(import.meta.env.VITE_TOP_SECRET);
   return <Navigation />;
 }
 
--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -5,27 +5,41 @@ import { Header } from './components/Header';
 import { Lists } from './components/Lists';
 
 function App() {
-const [todo, setTodo] = useState({
-  id: null,
-  name: '',
-  completed: false
-})
-const [todoList, setTodoList] = useState([])
-const [rename, setRename] = useState(null)
-const [message, setMessage] = useState('Press "Add" button to add a task.')
+  const [todo, setTodo] = useState({
+    id: null,
+    name: '',
+    completed: false,
+  });
+  const [todoList, setTodoList] = useState([]);
+  const [rename, setRename] = useState(null);
+  const [message, setMessage] = useState('Press "Add" button to add a task.');
 
-useEffect(() => {
-  if (localStorage.getItem('todoList')) {
-    setTodoList(JSON.parse(localStorage.getItem('todoList')))
-  }
-}, [])
+  useEffect(() => {
+    if (localStorage.getItem('todoList')) {
+      setTodoList(JSON.parse(localStorage.getItem('todoList')));
+    }
+  }, []);
+
+  console.log(process.env.REACT_APP_TOP_SECRET);
 
   return (
     <div>
-      <Header/>
+      <Header />
       <p>{message}</p>
-      <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList} rename={rename} setRename={setRename} setMessage={setMessage} />
-      <Lists todoList={todoList} setTodoList={setTodoList} setRename={setRename} />
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+        rename={rename}
+        setRename={setRename}
+        setMessage={setMessage}
+      />
+      <Lists
+        todoList={todoList}
+        setTodoList={setTodoList}
+        setRename={setRename}
+      />
     </div>
   );
 }
