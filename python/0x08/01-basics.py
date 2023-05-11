1bba8cb2b613468bd14783e0e27e068180501939
javierandresgp <info@javierandres.dev>
2023-05-11 15:27:39 -0500

    training

--- python/0x08/01-basics.py
+++ python/0x08/01-basics.py
@@ -2,4 +2,13 @@
 """
 Block comment
 """
-print('Hello, World!')
+print("Hello, World!")
+print("Hello\nWorld!")
+print("\tHello\n\tWorld!")
+print("\\tHello\\n\\tWorld!")
+my_str, my_num, my_bool, my_float = "Hi", 1, True, 0.5
+print("Formatting... %s %d %s %f" % (my_str, my_num, my_bool, my_float))
+print("Formatting... {} {} {} {}".format(my_str, my_num, my_bool, my_float))
+print(f"Formatting... {my_str} {my_num} {my_bool} {my_float}")
+a_name = input("Enter a name: ")
+print(a_name)
---
+++ python/0x08/05-strings.py
@@ -0,0 +1,20 @@
+# Strings
+word = "Hi"
+print(word)
+a, b = word
+print(a)
+print(b)
+print("works")
+print("works"[::-1])
+print("works"[:1])
+print("works"[1:])
+print("works"[-1])
+print("works"[1:4])
+print("works".capitalize())
+print("works".upper())
+print("works".lower())
+print("works".count("r"))
+print("works".isnumeric())
+print("works".islower())
+print("works".isupper())
+print("works".startswith("w"))
