0a8e83300ef5cb1dd5edd4a306c51ebbe9fa0e08
javierandresgp <info@javierandres.dev>
2023-05-20 16:24:33 -0500

    training

---
+++ python/0x08/07-tuples.py
@@ -0,0 +1,13 @@
+# tuples
+my_tuple = tuple()
+print(type(my_tuple))
+my_other_tuple = ()
+print(type(my_other_tuple))
+my_tuple = ("one", "two", "three", "four", "five")
+print(my_tuple)
+my_other_tuple = ("monday", "tuesday", " wednesday", "thursday", "friday")
+print(my_other_tuple)
+my_tuple = ("one", "two", "three", "four", "five", "one")
+print(my_tuple)
+print(my_tuple.index("three"))
+print(my_tuple.index("one"))
---
+++ python/0x08/08-sets.py
@@ -0,0 +1,7 @@
+# sets
+my_set = set()
+print(type(my_set))
+my_other_set = {}
+print(type(my_other_set))
+my_other_set = {"one", "two", "three", "four", "five"}
+print(type(my_other_set))
