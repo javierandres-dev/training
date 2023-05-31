a0e2fd974432a3020bf7aaf836f0ad7e2fdf94b2
javierandresgp <info@javierandres.dev>
2023-05-31 11:57:53 -0500

    training

--- react/previous/src/components/Cards.jsx
+++ react/previous/src/components/Cards.jsx
@@ -4,15 +4,18 @@ import { Cart } from './Cart';
 
 export const Cards = (props) => {
   const [cart, setCart] = useState([]);
-
-  const items = props.catalog.map((item) => (
-    <Card key={item.id} item={item} addToCart={addTocart} />
-  ));
+  const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [pay, setPay] = useState(null);
 
   useEffect(() => {
     const currentCart = JSON.parse(localStorage.getItem('myCart'));
-    if (currentCart.length > 0) {
+    if (currentCart && currentCart.length > 0) {
       setCart(currentCart);
+    }
+    const currentProducts = JSON.parse(localStorage.getItem('myProducts'));
+    if (currentProducts && currentProducts.length > 0) {
+      setProducts([...currentProducts]);
     }
   }, []);
 
@@ -20,14 +23,111 @@ export const Cards = (props) => {
     localStorage.setItem('myCart', JSON.stringify(cart));
   }, [cart]);
 
+  useEffect(() => {
+    localStorage.setItem('myProducts', JSON.stringify(products));
+    getTotal();
+  }, [products]);
+
   function addTocart(item) {
     setCart([...cart, item]);
+    updateProducts(item);
   }
+
+  function clearCart() {
+    setCart([]);
+    setProducts([]);
+    setTotal(0);
+    setPay(null);
+  }
+
+  function updateProducts(product) {
+    if (products.length === 0) {
+      const firstProduct = { ...product, quantity: 1 };
+      setProducts([...products, firstProduct]);
+    } else {
+      const index = products.findIndex((p) => p.id === product.id);
+      if (index === -1) {
+        const newProduct = { ...product, quantity: 1 };
+        setProducts([...products, newProduct]);
+      } else {
+        let arr = [...products];
+        const updateProduct = products[index];
+        updateProduct.quantity += 1;
+        arr.splice(index, 1, updateProduct);
+        setProducts([...arr]);
+      }
+    }
+  }
+
+  function getTotal() {
+    let sum = 0;
+    for (const i in products) {
+      const product = products[i];
+      sum += product.price * product.quantity;
+    }
+    setTotal(sum);
+  }
+
+  function handlePay() {
+    if (products.length === 0) {
+      alert('Empty cart');
+    } else {
+      const items = products.map((item) => (
+        <div key={item.id}>
+          {item.name} USD{item.price} x {item.quantity} = USD
+          {item.price * item.quantity}
+        </div>
+      ));
+
+      const thanks = (
+        <div className='pay'>
+          <button onClick={() => setPay(null)}>Close</button>
+          <div className='subtitle'>Tanks!</div>
+        </div>
+      );
+
+      const details = (
+        <div className='pay'>
+          <div className='subtitle'>Pay</div>
+          {items}
+          <div>Items: {cart.length}</div>
+          <div>Products: {products.length}</div>
+          <div>
+            <strong>Total: USD{total}</strong>
+          </div>
+          <button onClick={clearCart}>Cancel and clear cart</button>
+          <button onClick={() => setPay(null)}>Cancel</button>
+          <button
+            onClick={() => {
+              setCart([]);
+              setProducts([]);
+              setTotal(0);
+              setPay(thanks);
+            }}
+          >
+            Pay
+          </button>
+        </div>
+      );
+      setPay(details);
+    }
+  }
+
+  const items = props.catalog.map((item) => (
+    <Card key={item.id} item={item} addToCart={addTocart} />
+  ));
 
   return (
     <>
+      {pay && <div className='pay-container'>{pay}</div>}
       <div className='subtitle'>Catalog</div>
-      <Cart cart={cart} setCart={setCart} />
+      <Cart
+        totalItems={cart.length}
+        totalProducts={products.length}
+        total={total}
+        clearCart={clearCart}
+        handlePay={handlePay}
+      />
       <div className='cards'>{items}</div>
     </>
   );
--- react/previous/src/components/Cart.jsx
+++ react/previous/src/components/Cart.jsx
@@ -1,66 +1,21 @@
-import { useEffect, useState } from 'react';
-
-export const Cart = ({ cart, setCart }) => {
-  const [products, setProducts] = useState([]);
-  const [product, setProduct] = useState(null);
-  const [total, setTotal] = useState(0);
-
-  useEffect(() => {
-    getProducts();
-  }, [cart]);
-
-  useEffect(() => {
-    getTotal();
-  }, [products]);
-
-  useEffect(() => {
-    if (product !== null) {
-      updateProducts();
-    }
-  }, [product]);
-
-  function getProducts() {
-    cart.forEach((el) => {
-      const index = products.findIndex((p) => p.id === el.id);
-      if (index === -1) {
-        const newProduct = { ...el, quantity: 1 };
-        setProducts([...products, newProduct]);
-      } else {
-        setProduct(index);
-      }
-    });
-  }
-
-  function updateProducts() {
-    let arr = [...products];
-    const el = products[product];
-    el.quantity += 1;
-    arr.splice(product, 1, el);
-    setProduct(null);
-    setProducts([...arr]);
-  }
-
-  function getTotal() {
-    let sum = 0;
-    for (const i in products) {
-      const p = products[i];
-      sum += p.price * p.quantity;
-    }
-    setTotal(sum);
-  }
-
-  function handleClearCart() {
-    setCart([]);
-    setProducts([]);
-    setTotal(0);
-  }
-
+export const Cart = ({
+  totalItems,
+  totalProducts,
+  total,
+  clearCart,
+  handlePay,
+}) => {
   return (
     <div className='cart'>
-      <div>Cart items: {cart.length}</div>
-      <div>Products: {products.length}</div>
-      <div>Total: U$D{total}</div>
-      <button onClick={handleClearCart}>Clear cart</button>
+      <span>Items: {totalItems}</span>
+      <span>Products: {totalProducts}</span>
+      <span>Total: USD{total}</span>
+      {!total <= 0 && (
+        <>
+          <button onClick={clearCart}>Clear cart</button>
+          <button onClick={handlePay}>Pay</button>
+        </>
+      )}
     </div>
   );
 };
