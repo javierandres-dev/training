c4db82a28507203f031db42e51cf60913909812e
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-11-19 17:56:29 -0500

    training

---
+++ dom/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+const d = document,
+  $root = d.getElementById("root");
+
+let count = 0;
+
+d.addEventListener("DOMContentLoaded", () => {
+  createContent();
+});
+
+function createContent() {
+  const $h1 = d.createElement("h1"),
+    $img = d.createElement("img"),
+    $button = d.createElement("button");
+  $h1.textContent = "Welcome!";
+  $img.setAttribute("src", "./img/bulb-off.png");
+  $button.textContent = "ON / OFF";
+  $root.appendChild($h1);
+  $root.appendChild($img);
+  $root.appendChild($button);
+  changeContent($h1, $img, $button);
+}
+
+function changeContent($h1, $img, $button) {
+  $button.addEventListener("click", () => {
+    count++;
+    let img = $img.getAttribute("src");
+    img =
+      img === "./img/bulb-off.png"
+        ? (img = "./img/bulb-on.png")
+        : (img = "./img/bulb-off.png");
+    $img.setAttribute("src", img);
+    if (count === 1) $h1.textContent = "Works!";
+    if (count === 5) {
+      $h1.textContent = "Super!";
+      $h1.style.fontSize = "48px";
+      $h1.style.color = "white";
+    }
+  });
+}
