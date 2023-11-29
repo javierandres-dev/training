10eb5fa0d16ef278a6e7ed6703422a979fbf5469
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-11-29 14:22:50 -0500

    training

---
+++ javascript/0x00/main.js
@@ -0,0 +1,9 @@
+"use strict";
+import { add, substract, multiply, divide } from "./module.js";
+
+let result = null;
+result = add(8, 2);
+result = substract(8, 2);
+result = multiply(8, 2);
+result = divide(8, 2);
+console.log(result);
---
+++ javascript/0x00/module.js
@@ -0,0 +1,5 @@
+"use strict";
+export const add = (a, b) => a + b;
+export const substract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
