ab8ed849ac1663c95531a8708e369a8e9e030fc3
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-02-01 18:10:26 -0500

    training

--- exercises/0x00/hlgbtq+/script.js
+++ exercises/0x00/hlgbtq+/script.js
@@ -4,11 +4,13 @@ const $startModal = new bootstrap.Modal(d.getElementById("startModal"));
 const $doNotAsk = d.getElementById("doNotAsk");
 const $ask = d.getElementById("ask");
 const $testForm = d.getElementById("testForm");
+const $select = d.getElementById("0");
 
 d.addEventListener("DOMContentLoaded", () => {
   $startModal.show();
 });
 
+$select.addEventListener("change", enableRadios);
 $testForm.addEventListener("submit", (e) => handleSubmit(e));
 
 function handleNo() {
@@ -21,15 +23,64 @@ function handleYes() {
   $startModal.hide();
 }
 
+function enableRadios() {
+  const $radios = d.querySelectorAll("input[type='radio']");
+  $radios.forEach((radio) => {
+    radio.removeAttribute("disabled");
+    radio.addEventListener("change", () => {
+      handleCheckboxes(radio.id);
+    });
+  });
+}
+
+function handleCheckboxes(id) {
+  const $checkboxes = d.querySelectorAll("input[type='checkbox']");
+  if (id === "1")
+    for (const checkbox of $checkboxes) checkbox.removeAttribute("disabled");
+  else
+    for (const checkbox of $checkboxes) {
+      checkbox.checked = false;
+      checkbox.setAttribute("disabled", true);
+    }
+}
+
 function handleSubmit(e) {
   e.preventDefault();
-  const obj = {};
+  const data = {};
   const formData = new FormData($testForm);
   for (const [key, value] of formData) {
-    console.log("key:", key, " value:", value + "\n");
-    obj[key] = value === "true" ? Boolean(value) : value;
+    data[key] = value === "true" ? true : value === "false" ? false : value;
   }
-  console.log("obj:", obj);
-  let result = `You are a ${obj.biologicalSex}`;
+  console.log("data:", data);
+  let attraction = "";
+  if (data.likeFemale) attraction += "female";
+  if (data.likeMale) attraction += attraction ? " and male" : "male";
+  let genderIdentity = "";
+  let genderDetail = "";
+  if (data.biologicalSex === "female") {
+    if (data.likeFemale && data.likeMale) {
+      genderIdentity = "bisexual";
+      genderDetail = "person attracted to both sexes";
+    } else if (data.likeFemale) {
+      genderIdentity = "lesbian";
+      genderDetail = "homosexual person, woman attracted to women";
+    } else {
+      genderIdentity = "heterosexual";
+      genderDetail = "person attracted to opposite sex";
+    }
+  }
+  if (data.biologicalSex === "male") {
+    if (data.likeFemale && data.likeMale) {
+      genderIdentity = "bisexual";
+      genderDetail = "person attracted to both sexes";
+    } else if (data.likeMale) {
+      genderIdentity = "gay";
+      genderDetail = "homosexual person, man attracted to men";
+    } else {
+      genderIdentity = "heterosexual";
+      genderDetail = "person attracted to opposite sex";
+    }
+  }
+  let result = `You are a ${data.biologicalSex}, and you feel romantically and physically attraction to ${attraction}, your gender identity is ${genderIdentity} (${genderDetail}).`;
   console.log("result:", result);
 }
