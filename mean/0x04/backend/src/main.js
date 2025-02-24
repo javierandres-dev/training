b0d98a691818df9c59382b773331199613e955b2
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-02-23 22:44:53 -0500

    training

--- mean/0x04/backend/src/main.js
+++ mean/0x04/backend/src/main.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const app = express();
+import express from "express";
+
+const server = express();
 const port = 4100;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+try {
+  //console.log("works!");
+  server.get("/", (req, res) => {
+    res.json({ message: "root works!" });
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  server.listen(port, () => {
+    console.log(`Server is runnig and listening on port ${port}`);
+  });
+} catch (error) {
+  console.log("Server not running! Error:", error);
+} finally {
+  console.log("Done!");
+}
