aba1c60a24d5cc78bea890dcd077dd4607098590
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-03-26 16:18:05 -0500

    training

--- html-css-js/0x00/js/main.js
+++ html-css-js/0x00/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+function handleSubmit(event) {
+  event.preventDefault();
+  //alert("Submitted! event:", event);
+  //console.log("event:", event);
+  //console.log("event:", event.target);
+  //console.log("event:", event.target.elements);
+  //console.log("event:", event.target.elements.name);
+  /* console.log("event:", event.target.elements.name.value);
+  console.log("event:", event.target.elements.email.value);
+  console.log("event:", event.target.elements.message.value); */
+  const notify = `Name: ${event.target.elements.name.value}\nEmail: ${event.target.elements.email.value}\nMessage: ${event.target.elements.message.value}`;
+  alert("Submitted!\n\n" + notify + "\n\nWorks!");
+  const $form = document.querySelector("form");
+  $form.reset();
+}
