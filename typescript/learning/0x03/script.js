adc3b1e39be28cde5f3c8c896872ab9184cef64c
javi <55107141+javi0b01@users.noreply.github.com>
2025-06-04 14:47:39 -0500

    training

--- typescript/learning/0x03/script.js
+++ typescript/learning/0x03/script.js
@@ -1,66 +1,33 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-// abstract class
-var Animal = /** @class */ (function () {
-    function Animal() {
-    }
-    return Animal;
-}());
-var Cat = /** @class */ (function (_super) {
-    __extends(Cat, _super);
-    function Cat() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Cat.prototype.sound = function () {
+"use strict";
+class Animal {
+}
+class Cat extends Animal {
+    sound() {
         console.log("meow");
-    };
-    return Cat;
-}(Animal));
-var Dog = /** @class */ (function (_super) {
-    __extends(Dog, _super);
-    function Dog() {
-        return _super !== null && _super.apply(this, arguments) || this;
     }
-    Dog.prototype.sound = function () {
+}
+class Dog extends Animal {
+    sound() {
         console.log("woof");
-    };
-    return Dog;
-}(Animal));
-var cat = new Cat();
-var dog = new Dog();
+    }
+}
+const cat = new Cat();
+const dog = new Dog();
 cat.sound();
 dog.sound();
-var Horse = /** @class */ (function (_super) {
-    __extends(Horse, _super);
-    function Horse() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Horse.prototype.sound = function () {
+class Horse extends Animal {
+    sound() {
         console.log("neigh");
-    };
-    Horse.prototype.go = function (how) {
-        return "I'm ".concat(how);
-    };
-    return Horse;
-}(Animal));
-var horse1 = new Horse();
-var horse2 = new Horse();
+    }
+    go(how) {
+        return `I'm ${how}`;
+    }
+}
+const horse1 = new Horse();
+const horse2 = new Horse();
 console.log(horse1.go("walking"));
 console.log(horse2.go("runnig"));
-var contact = {
+const contact = {
     name: "jane",
     phone: "555",
 };
--- typescript/learning/index.js
+++ typescript/learning/index.js
@@ -1,18 +1,11 @@
-/* Benefits of TS:
-better error feedback,
-better autocompletion,
-code hints
-self documenting
-tipado */
-// Primitives
-var myString = "1";
-var myNumber = 1;
-var myBoolean = true;
+"use strict";
+let myString = "1";
+let myNumber = 1;
+let myBoolean = true;
 myString = 1;
 myNumber = "1";
 myBoolean = "false";
-// Specials
-var myNull = null;
-var myUndefined = undefined;
+let myNull = null;
+let myUndefined = undefined;
 myNull = 1;
 myUndefined = 1;
